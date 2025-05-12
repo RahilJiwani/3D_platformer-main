435b391cc559c3c512e0282a19e6e93cdab345ba
RahilJiwani <95107310+RahilJiwani@users.noreply.github.com>
2025-05-12 15:54:38 -0400

    jumping

--- 3D_platformer-main/Assets/MovementScript.cs
+++ 3D_platformer-main/Assets/MovementScript.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Android;
 
 public class MovementScript : MonoBehaviour
 {
 
     public float speed; // set to 5 in the Unity Inspector
     public float rotationSpeed;
+    public float jumpSpeed;
+    private float ySpeed;
+    private CharacterController conn;
+    public bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        conn = GetComponent<CharacterController>();                 //the predefined component CharacterController handles collisions, slopes, stairs, etc., for you.
     }
 
     // Update is called once per frame
@@ -28,9 +33,32 @@ public class MovementScript : MonoBehaviour
        
         float magnitude = moveDirection.magnitude; // Get the magnitude of the vector (length)
         magnitude = Mathf.Clamp(magnitude, 0 , 1); // Clamps the magnitude to a maximum of 1 to prevent faster diagonal movement.
-        
+
+        conn.SimpleMove(moveDirection * magnitude * speed);
         
         transform.Translate(moveDirection * speed * Time.deltaTime, Space.World); // space defines what the player is relative to 
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;       //sets fall speed equal to gravity and accelerateds by 1/framerate
+        if (Input.GetButtonDown("Jump")){                   //if jump button is pressed do 
+            ySpeed = -0.5f;                                    //yspeed is a measure of downwards movement (fallinf by gravity), so a negative number is an upwards nudge (jump). f just sets it to a float value
+            isGrounded = false;
+        }
+
+        Vector3 vel = moveDirection * magnitude;            //velocity is a vector, move direction accounts for the direction player is moving in and magnitude accounts for the magnitude portion (how fast player is moving in that direction)
+        vel.y = ySpeed;                                     //Injects your vertical velocity (the result of gravity + jump) into the Y component of that velocity vector.
+        //transform.Translate(vel * Time.deltaTime);          //sets translate (players' movement in the game space) to the velocity we just calculated.
+        conn.Move(vel * Time.deltaTime);
+
+        if(conn.isGrounded)
+        {
+            ySpeed = -0.5f;
+            isGrounded = true;
+            if (Input.GetButtonDown("Jump"))
+            {
+                ySpeed = jumpSpeed;
+                isGrounded=false;
+            }
+        }
 
         if (moveDirection != Vector3.zero) // Now the player can rotate to face the direction of movement, he won't snap back to default look direction
         {
